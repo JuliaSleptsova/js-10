e09aae96662de12b1cbc053b4c5158ab260e37b0
juliaSleptsova <eliseyka842@gmail.com>
2023-02-25 13:16:24 +0200

    hm18

--- script.js
+++ script.js
@@ -1,114 +1,64 @@
+// pythagorean
+const a = 5;
+const b = 12;
+const c = pythagorean(a, b);
 
-// sort arr str
-const arr = ["4534", "a", "bb", "sdfds", "", " ", "r4rdv-"];
-arr.sort(function(a, b) {
-  if (a > b) {
-        return 1;
-    }
-    if (b > a) {
-        return -1;
-    }
-    return 0;
-});
+function pythagorean(a, b) {
+  const c = Math.sqrt(a*a + b*b);
+  return c;
+}
+console.log(c);
 
-console.log(arr); 
+// integer and demical part of number
+const userNumber = prompt("Enter a number:");
+const number = parseFloat(userNumber); 
+if (isNaN(number)) {
+  console.log("Wrong number");
+} else {
+  const integerPart = Math.floor(number);
+  const decimalPart = (number - integerPart).toFixed(2);
 
-// sort arr by name
-const arr = [
-  { name: "John", age: 25 },
-  { name: "John", age: 5 },
-  { name: "John", age: 2 },
-  { name: "John", age: 45 },
-  { name: "Pete", age: 30 },
-  { name: "Mary", age: 29 },
-  { name: "Mary", age: 2 },
-  { name: "Taras", age: 25 },
-  { name: "Taras", age: 19 },
-  { name: "Kate", age: 74 },
-  { name: "Chris", age: 14 },
-  { name: "Alan", age: 5 },
-  { name: "Alan", age: 32 },
-  { name: "Boris", age: 55 },
-  { name: "Elizabeth", age: 48 },
-  { name: "Elizabeth", age: 8 },
-];
+alert(`Integer: ${integerPart}`);
+alert(`Decimal: ${decimalPart}`);
+}
 
-const sortUsers = arr.sort (function (a, b) {
-     if (a.name < b.name) {
-    return -1;
-  } else if (a.name > b.name) {
-    return 1;
-  } else {
-    return a.age - b.age;
+// Write a password generator with length n
+function generatePassword(n) {
+  const regex =
+    /[\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/0-9\:\;\<\=\>\?\@\[A-Z\]\^_\`\[a-z\]]/g;
+  let password = "";
+  for (let i = 0; i < n; i++) {
+    password += String.fromCharCode(
+      Math.floor(Math.random() * (126 - 33 + 1)) + 33
+    ).match(regex);
   }
-})
-
-console.log(sortUsers);
-
-
-// sort arr
-const arr = [324, 32423, -3242, 544, 0, 23, -454, 22, 4];
-arr.sort(function(a, b) {
-  return a - b;
-});
-console.log(arr);
-
-// filter arr
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-function filterRange(arr, from, to) {
-  return arr.filter(num => num >= from && num <= to);
+  return password;
 }
-const filteredArr = filterRange(arr, 3, 7);
-console.log(filteredArr); 
+console.log(generatePassword(8));
 
-// capitalize words
-const str = 'my name is julia';
-const arr = str.split(" ");
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+// calculate percentage with n numbers after dot
+function calculate(firstNum, secondNum, precision) {
+  let calc = (secondNum * 100) / firstNum;
+  const percentage =
+    Math.round(calc * Math.pow(10, precision)) / Math.pow(10, precision);
+  return percentage;
 }
-const str2 = arr.join(" ");
-console.log(str2);
+console.log(calculate(200, 0.14, 1));
+console.log(calculate(200, 50, 0));
 
-// avarage age
-const arr = [
-  { name: "John", age: 25 },
-  { name: "Pete", age: 30 },
-  { name: "Mary", age: 29 },
-  { name: "Taras", age: 25 },
-  { name: "Kate", age: 74 },
-  { name: "Chris", age: 14 },
-  { name: "Alan", age: 5 },
-  { name: "Boris", age: 55 },
-  { name: "Elizabeth", age: 48 },
-];
+// Check if prime
+function isPrime(n) {
+  if (n < 2 || !Number.isInteger(n)) {
+    return false;
+  }
 
-const averageAge = arr.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue.age;
-}, 0);
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
 
-console.log(averageAge / arr.length);
-
-// find mix and max
-const arr = [
-  { name: "John", age: 25 },
-  { name: "Pete", age: 30 },
-  { name: "Mary", age: 29 },
-  { name: "Taras", age: 25 },
-  { name: "Kate", age: 74 },
-  { name: "Chris", age: 14 },
-  { name: "Alan", age: 5 },
-  { name: "Boris", age: 55 },
-  { name: "Elizabeth", age: 48 },
-];
-
-const maxAge = arr.reduce(function (previous, current) {
-    return current.age > previous.age ? current : previous;
-});
-
-const minAge = arr.reduce(function (previous, current) {
-    return current.age < previous.age ? current : previous;
-});
-console.log (minAge);
-console.log (maxAge);
+  return true;
+}
+console.log(isPrime(4));
+console.log(isPrime(5));
